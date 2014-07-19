ee05ce1031a94d8f20177b4dcdb3b5b8eb6e5c5c
suras <msurendar@outlook.com>
2014-07-19 21:00:38 +0530

    search api changes

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -7,15 +7,18 @@ class Api::V1::SearchController < Api::V1::BaseController
   	longitude = params[:search][:longitude].to_f
   	if(params[:search][:listing_category].present?)
   	  list_cat_ids = ListingCategory.where(name: /#{params[:search][:listing_category]}/)
-      @services = Service.where(:list_cat_id.in => list_cat_ids).geo_near([latitude, longitude]).max_distance(params[:search][:distance]).page(params[:page]).per(params[:per])
+      list_cat_ids = list_cat_ids.map{|l| l.id.to_s}
+      @services = Service.where(:list_cat_ids.in => list_cat_ids).page(params[:page]).per(params[:per]).geo_near([latitude, longitude]).max_distance(params[:search][:distance])
+  	  @services = @services.to_a
   	elsif(params[:search][:service_name].present?)
-      @services = Service.where(:business_name => /#{params[:search][:service_name]}/).geo_near([latitude, longitude]).max_distance(params[:search][:distance])
+      @services = Service.where(:business_name => /#{params[:search][:service_name]}/).page(params[:page]).per(params[:per]).geo_near([latitude, longitude]).max_distance(params[:search][:distance])
+  	  @services = @services.to_a
   	else
       @services = Service.all.page(params[:page]).per(params[:per])
   	end
-  	render json: @services
+  	render json: @services, each_serializer: ServiceSerializer
   rescue => e
-     render json: {error_code: Code[:error_rescue], error_message: e.message}, status: Code[:status_error]
+      render json: {error_code: Code[:error_rescue], error_message: e.message}, status: Code[:status_error]
   end
 
 end
