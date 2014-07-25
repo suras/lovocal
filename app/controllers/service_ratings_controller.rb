5987140e8c4c74550217283b5c40627d1c299462
suras <msurendar@outlook.com>
2014-07-25 19:53:08 +0530

    services web controller

--- app/controllers/service_ratings_controller.rb
+++ app/controllers/service_ratings_controller.rb
@@ -1,2 +1,72 @@
 class ServiceRatingsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_service, only: [:create, :index]
+
+  # GET /services/service_rating
+  def index
+    @service_ratings = @service.service_ratings
+  end
+
+  # POST /services/service_rating
+  def create 
+    @service_rating = @service.service_ratings.new(service_rating_params)
+    @service_rating.user = current_user
+    respond_to do |format|
+      if(@service_rating.save)
+        format.html { redirect_to @service_rating, notice: 'Service Rating was successfully created.' }
+        format.json { render :show, status: :created, location: @service_rating }
+      else
+        format.html { render :new }
+        format.json { render json: @service_rating.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET  /services/:service_id/service_rating/
+  def show
+    @service_rating = @service.service_ratings.find(params[:id])
+  end
+
+  # GET  /services/:service_id/service_rating/new
+  def new
+    @service_rating = @service.service_ratings.new
+  end
+  
+  # GET /services/:service_id/service_rating/edit
+  def edit
+    @service_rating = current_user.service_ratings.find(params[:id])
+  end
+
+  # PATCH/PUT /services/:service_id/service_rating
+  def update
+  	@service_rating = current_user.service_ratings.find(params[:id])
+    respond_to do |format|
+      if(@service_rating.update_attributes(service_rating_params))
+        format.html { redirect_to @service_rating, notice: 'Service rating was successfully updated.' }
+        format.json { render :show, status: :ok, location: @service_rating }
+      else
+        format.html { render :edit }
+        format.json { render json: @service_rating.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /services/:service_id/service_rating
+  def destroy
+    @service_rating = current_user.service_ratings.find(params[:id])
+    @service_rating.destroy
+    render json: { head: :no_content}
+  end
+
+  private
+  
+  def set_service
+  	return nil if params[:service_id].blank?
+    @service = Service.find(params[:service_id])
+  end
+
+  def service_rating_params
+    params.require(:service_rating).permit(:comment, :rating)
+  end
+
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,141 @@
 class ServicesController < ApplicationController
+before_action :authenticate_user!
+  
+  # POST /listing_category
+  def create 
+    @service = current_user.services.new(service_params)
+    @service.listing = Listing.where(listing_type: "services").first
+    respond_to do |format|
+      if @service.save
+        format.html { redirect_to @service, notice: 'service was successfully created.' }
+        format.json { render :show, status: :created, location: @service }
+      else
+        format.html { render :new }
+        format.json { render json: @service.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @service = current_user.services.find(params[:id])
+  end
+
+  def show
+    @service = current_user.services.find(params[:id])
+  end
+
+  def new
+    @service = current_user.services.new
+  end
+
+  # PATCH/PUT /services
+  def update
+  	@service = currrent_user.services.find(params[:id])
+    respond_to do |format|
+      if @service.update(service_params)
+        format.html { redirect_to @service, notice: 'service was successfully updated.' }
+        format.json { render :show, status: :ok, location: @service }
+      else
+        format.html { render :edit }
+        format.json { render json: @service.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /services
+  def destroy
+    @service = current_user.services.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to services_url, notice: 'Service was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  # DELETE /services/:service_id/service_images
+  def destroy_images
+    @service = current_user.services.find(params[:service_id])
+    @service.service_images.where(:_id.in => params[:service_image][:ids]).destroy   
+    respond_to do |format|
+      format.html { redirect_to @service, notice: 'Images was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def new_images
+    @service = current_user.services.find(params[:id])
+  end
+  # POST /services/:service_id/service_images
+  def create_images
+    @service = current_user.services.find(params[:service_id])
+    params[:service_image][:image].each do |image|
+      @service.service_images.create!(image: image)
+    end
+    respond_to do |format|
+      format.html { redirect_to @service, notice: 'Images was successfully created.' }
+      format.json { json: @service, status: :ok, location: @service  }
+    end
+  rescue => e
+  	respond_to do |format|
+      format.html { render :create_new_images}
+      format.json { json: @service.errors, status: :unprocessable_entity}
+    end
+  end
+
+  def timing
+    @service = current_user.services.find(params[:service_id])
+    @service_timing = @service.service_timing
+    unless(@service.service_timing.present?)
+      @service_timing = @service.build_service_timing
+    end
+  end
+
+  # POST services/service_id/service_timings
+  def create_timings
+    @service = current_user.services.find(params[:service_id])
+    @service_timing = @service.build_service_timing
+    @service_timing.timings = {}
+    timing_pair = JSON.parse(params[:service_timing][:timings])
+    timing_pair.each_pair do |day, time|
+      @service_timing.timings[day] = time
+    end
+    @service_timing.holidays = params[:service_timing][:holidays]
+    respond_to do |format|
+      if(@service_timing.save)
+        format.html { redirect_to @service_timing, notice: 'service was successfully updated.' }
+        format.json { render :show, status: :ok, location: @service }
+      else
+        format.html { render :timing }
+        format.json { render json: @service_timing.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /services/:id/rating
+  def rating
+    @service  =  Service.find(params[:id])
+    avg = @service.avg_rating
+  	respond_to do |format|
+      format.html { redirect_to @service}
+      format.json { json: avg, status: :ok}
+    end 
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def service_params
+      params.require(:service).permit(:business_name, :mobile_number, :landline_number, 
+      	:email, :description, :customer_care_no, :latitude, :address, {list_cat_ids: []},
+      	:longitude, :country, :state, :city, :zip_code, :website, :facebook_link,
+        :twitter_link, :linkedin_link, {service_images_attributes: [:image, :is_main]}
+      	 )
+    end
+
+    def get_list_cat_ids_by_name
+	  ids = Array.new
+	  params[:service][:listing_categories].try(:each) do |cat|
+        list_cat = ListingCategory.where(name: cat).first
+        ids << list_cat.id.to_s if list_cat.present?
+	  end
+	  return ids
+    end
 end
